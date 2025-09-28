94d058b87920177627dba4fea2e1671b24786b18
Lukas <wi23b019@technikum-wien.at>
2025-09-28 13:11:36 +0200

    camera control + zooming
    added camera control through arrow keys and zooming through mouse wheel

--- game/scripts/ui/camera.gd
+++ game/scripts/ui/camera.gd
@@ -1,5 +1,16 @@
 extends Camera2D
 
+#Camera Control
+@export var SPEED = 20.0
+@export var ZOOM_SPEED = 20.0
+@export var ZOOM_MARGIN = 0.1
+@export var ZOOM_MIN = 0.5
+@export var ZOOM_MAX = 3.0
+
+var zoomFactor = 1.0
+var zoomPos = Vector2()
+var zooming = false
+
 var mousePos = Vector2()
 
 var mousePosGlobal = Vector2()
@@ -18,6 +29,22 @@ func _ready():
 	connect("area_selected", Callable(get_parent(), "_on_area_selected"))
 
 func _process(delta):
+	
+	var inputX = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	var inputY = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	
+	position.x  =lerp(position.x, position.x + inputX * SPEED * zoom.x, SPEED * delta)
+	position.y  =lerp(position.y, position.y + inputY * SPEED * zoom.y, SPEED * delta)
+	
+	zoom.x = lerp(zoom.x, zoom.x * zoomFactor, ZOOM_SPEED * delta)
+	zoom.y = lerp(zoom.y, zoom.y * zoomFactor, ZOOM_SPEED * delta)
+	
+	zoom.x = clamp(zoom.x, ZOOM_MIN, ZOOM_MAX)
+	zoom.y = clamp(zoom.y, ZOOM_MIN, ZOOM_MAX)
+	
+	if not zooming:
+		zoomFactor = 1.0
+	
 	if Input.is_action_just_pressed("LeftClick"):
 		start = mousePosGlobal
 		startV = mousePos
@@ -41,6 +68,23 @@ func _process(delta):
 			draw_area(false)
 
 func _input(event):
+	if abs(zoomPos.x - get_global_mouse_position().x) > ZOOM_MARGIN:
+		zoomFactor = 1.0
+	if abs(zoomPos.y - get_global_mouse_position().y) > ZOOM_MARGIN:
+		zoomFactor = 1.0 
+		
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			zooming = true
+			if event.is_action("WheelDown"):
+				zoomFactor -= 0.01 * ZOOM_SPEED
+				zoomPos = get_global_mouse_position()
+			if event.is_action("WheelUp"):
+				zoomFactor += 0.01 * ZOOM_SPEED
+				zoomPos = get_global_mouse_position()	
+		else:
+			zooming = false
+			
 	if event is InputEventMouse:
 		mousePos = event.position
 		mousePosGlobal = get_global_mouse_position()
